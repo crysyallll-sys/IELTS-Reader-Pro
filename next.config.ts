8e1f960c30e6a3e2a7aae057ec69396d0c0ce1e5
jiaqi <linhd@linhddeMacBook-Pro.local>
2026-05-14 16:37:35 +0800

    改为静态导出模式

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
